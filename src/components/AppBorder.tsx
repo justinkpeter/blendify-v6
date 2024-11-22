4d54f09e8c859e987cbdd99d8a5ef644c6afaadd
Justin <justinkmpeter@gmail.com>
2024-11-21 18:25:43 -0600

    removing  app border and reworking page layout

--- src/components/AppBorder.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "@/styles/components/AppBorder.module.scss";
-
-export default function AppBorder({ children }: { children: React.ReactNode }) {
-  return <div className={styles.appBorder}>{children}</div>;
-}
