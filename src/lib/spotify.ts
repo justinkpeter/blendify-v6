711a176969d5f0c7f0f67728512b6d6db700bb4b
Justin <justinkmpeter@gmail.com>
2024-12-13 16:57:44 -0600

    updating spotify

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,3 +1,5 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
 const scopes = [
   "user-top-read",
   "user-library-read",
@@ -15,5 +17,23 @@ const params = {
 
 const queryParamString = new URLSearchParams(params).toString();
 const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamString}`;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: `${process.env.NEXTAUTH_URL}/api/auth/callback/spotify`,
+});
+
+export async function getAccessToken() {
+  try {
+    const data = await spotifyApi.clientCredentialsGrant();
+    spotifyApi.setAccessToken(data.body.access_token);
+    return data.body.access_token;
+  } catch (error) {
+    console.error("Error retrieving Spotify access token", error);
+  }
+}
+
+export default spotifyApi;
 
 export { LOGIN_URL };
