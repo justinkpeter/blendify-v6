18b46cb5d964b7f2a4a0b89557c89413ef7ff56f
Justin <justinkmpeter@gmail.com>
2025-01-18 00:24:33 -0600

    taste profile

---
+++ src/components/Barcode.tsx
@@ -0,0 +1,79 @@
+export default function Barcode() {
+  return (
+    <svg
+      width="414"
+      height="25"
+      viewBox="0 0 414 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M3.39344 0H0V25H3.39344V0Z" fill="#BDBDBD" />
+      <path d="M6.78687 0H5.09015V25H6.78687V0Z" fill="#BDBDBD" />
+      <path d="M11.877 0H10.1803V25H11.877V0Z" fill="#BDBDBD" />
+      <path d="M20.3606 0H18.6639V25H20.3606V0Z" fill="#BDBDBD" />
+      <path d="M25.4508 0H23.7541V25H25.4508V0Z" fill="#BDBDBD" />
+      <path d="M35.6311 0H32.2377V25H35.6311V0Z" fill="#BDBDBD" />
+      <path d="M40.7213 0H37.3279V25H40.7213V0Z" fill="#BDBDBD" />
+      <path d="M45.8115 0H44.1147V25H45.8115V0Z" fill="#BDBDBD" />
+      <path d="M49.2049 0H47.5082V25H49.2049V0Z" fill="#BDBDBD" />
+      <path d="M57.6885 0H55.9918V25H57.6885V0Z" fill="#BDBDBD" />
+      <path d="M62.7787 0H59.3853V25H62.7787V0Z" fill="#BDBDBD" />
+      <path d="M67.8688 0H66.1721V25H67.8688V0Z" fill="#BDBDBD" />
+      <path d="M78.0492 0H74.6557V25H78.0492V0Z" fill="#BDBDBD" />
+      <path d="M86.5328 0H84.8361V25H86.5328V0Z" fill="#BDBDBD" />
+      <path d="M89.9262 0H88.2295V25H89.9262V0Z" fill="#BDBDBD" />
+      <path d="M95.0164 0H93.3197V25H95.0164V0Z" fill="#BDBDBD" />
+      <path d="M103.5 0H101.803V25H103.5V0Z" fill="#BDBDBD" />
+      <path d="M110.287 0H106.893V25H110.287V0Z" fill="#BDBDBD" />
+      <path d="M113.68 0H111.984V25H113.68V0Z" fill="#BDBDBD" />
+      <path d="M123.861 0H120.467V25H123.861V0Z" fill="#BDBDBD" />
+      <path d="M127.254 0H125.557V25H127.254V0Z" fill="#BDBDBD" />
+      <path d="M132.344 0H130.648V25H132.344V0Z" fill="#BDBDBD" />
+      <path d="M137.434 0H134.041V25H137.434V0Z" fill="#BDBDBD" />
+      <path d="M145.918 0H144.221V25H145.918V0Z" fill="#BDBDBD" />
+      <path d="M152.705 0H149.311V25H152.705V0Z" fill="#BDBDBD" />
+      <path d="M157.795 0H154.402V25H157.795V0Z" fill="#BDBDBD" />
+      <path d="M166.279 0H159.492V25H166.279V0Z" fill="#BDBDBD" />
+      <path d="M171.369 0H167.975V25H171.369V0Z" fill="#BDBDBD" />
+      <path d="M176.459 0H173.066V25H176.459V0Z" fill="#BDBDBD" />
+      <path d="M183.246 0H179.852V25H183.246V0Z" fill="#BDBDBD" />
+      <path d="M188.336 0H186.639V25H188.336V0Z" fill="#BDBDBD" />
+      <path d="M198.516 0H195.123V25H198.516V0Z" fill="#BDBDBD" />
+      <path d="M201.91 0H200.213V25H201.91V0Z" fill="#BDBDBD" />
+      <path d="M207 0H205.303V25H207V0Z" fill="#BDBDBD" />
+      <path d="M215.484 0H208.697V25H215.484V0Z" fill="#BDBDBD" />
+      <path d="M220.574 0H218.877V25H220.574V0Z" fill="#BDBDBD" />
+      <path d="M227.361 0H223.967V25H227.361V0Z" fill="#BDBDBD" />
+      <path d="M232.451 0H229.057V25H232.451V0Z" fill="#BDBDBD" />
+      <path d="M239.238 0H235.844V25H239.238V0Z" fill="#BDBDBD" />
+      <path d="M244.328 0H242.631V25H244.328V0Z" fill="#BDBDBD" />
+      <path d="M249.418 0H247.721V25H249.418V0Z" fill="#BDBDBD" />
+      <path d="M254.508 0H251.115V25H254.508V0Z" fill="#BDBDBD" />
+      <path d="M268.082 0H261.295V25H268.082V0Z" fill="#BDBDBD" />
+      <path d="M273.172 0H271.475V25H273.172V0Z" fill="#BDBDBD" />
+      <path d="M276.566 0H274.869V25H276.566V0Z" fill="#BDBDBD" />
+      <path d="M281.656 0H279.959V25H281.656V0Z" fill="#BDBDBD" />
+      <path d="M286.746 0H283.352V25H286.746V0Z" fill="#BDBDBD" />
+      <path d="M291.836 0H290.139V25H291.836V0Z" fill="#BDBDBD" />
+      <path d="M300.32 0H298.623V25H300.32V0Z" fill="#BDBDBD" />
+      <path d="M308.803 0H302.016V25H308.803V0Z" fill="#BDBDBD" />
+      <path d="M313.893 0H312.197V25H313.893V0Z" fill="#BDBDBD" />
+      <path d="M318.984 0H317.287V25H318.984V0Z" fill="#BDBDBD" />
+      <path d="M330.861 0H324.074V25H330.861V0Z" fill="#BDBDBD" />
+      <path d="M334.254 0H332.557V25H334.254V0Z" fill="#BDBDBD" />
+      <path d="M342.738 0H335.951V25H342.738V0Z" fill="#BDBDBD" />
+      <path d="M349.525 0H344.434V25H349.525V0Z" fill="#BDBDBD" />
+      <path d="M352.918 0H351.221V25H352.918V0Z" fill="#BDBDBD" />
+      <path d="M356.311 0H354.615V25H356.311V0Z" fill="#BDBDBD" />
+      <path d="M361.402 0H358.008V25H361.402V0Z" fill="#BDBDBD" />
+      <path d="M366.492 0H364.795V25H366.492V0Z" fill="#BDBDBD" />
+      <path d="M378.369 0H373.279V25H378.369V0Z" fill="#BDBDBD" />
+      <path d="M381.762 0H380.066V25H381.762V0Z" fill="#BDBDBD" />
+      <path d="M390.246 0H386.852V25H390.246V0Z" fill="#BDBDBD" />
+      <path d="M395.336 0H391.943V25H395.336V0Z" fill="#BDBDBD" />
+      <path d="M405.516 0H400.426V25H405.516V0Z" fill="#BDBDBD" />
+      <path d="M408.91 0H407.213V25H408.91V0Z" fill="#BDBDBD" />
+      <path d="M414 0H410.607V25H414V0Z" fill="#BDBDBD" />
+    </svg>
+  );
+}
